e798407b35bfca6aec0c06b376962e1e8bca430a
Max Franke <m.franke@campus.tu-berlin.de>
2016-05-06 21:18:12 +0200

    Re-Added completed to Model

--- todos/models.py
+++ todos/models.py
@@ -3,10 +3,12 @@ from django.core.validators import MaxValueValidator
 
 class Todo(models.Model):
     description = models.CharField(max_length=100)
+    completed = models.BooleanField(default = False)
     completion = models.PositiveIntegerField(default=0, 
         validators=[
             MaxValueValidator(100)
         ])
+    
     due_date = models.DateField(default = '2017-1-1')
 
     def __str__(self):
