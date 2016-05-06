1e91757dab6d45a432a9386b2d1c56c13e3da0cb
Leonardo Hahn <leo_lh324@hotmail.com>
2016-05-06 21:31:23 +0200

    using timezone.now and renamed completion to progress

--- todos/models.py
+++ todos/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.utils import timezone
 from django.core.validators import MaxValueValidator
 
 class Todo(models.Model):
     description = models.CharField(max_length=100)
     completed = models.BooleanField(default = False)
-    completion = models.PositiveIntegerField(default=0, 
+    progress = models.PositiveIntegerField(default=0,
         validators=[
             MaxValueValidator(100)
         ])
-    
-    due_date = models.DateField(default = '2017-1-1')
+
+    due_date = models.DateField(default=timezone.now)
 
     def __str__(self):
         status = 'V' if self.completed else 'X'
