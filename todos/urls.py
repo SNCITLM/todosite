450bb5179d1b0c16e4e407b03575bd8f8c1197f6
Leonardo Hahn <leo_lh324@hotmail.com>
2016-05-06 17:05:41 +0200

    views working and also database

--- todos/urls.py
+++ todos/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<todo_id>[0-9]+)/edit$', views.edit, name='edit'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# This views renders all of the current todos on screen.
 def index(request):
     return HttpResponse("Hello, world. You're at the todos index.")
+
+def edit(request, todo_id):
+    return HttpResponse('Hey, you want to edit todo %s' % todo_id)
