6f6728617447451320b2055d42cd4e26007ed85c
Leonardo Hahn <leo_lh324@hotmail.com>
2016-05-06 21:39:32 +0200

    added index.html view

--- todos/views.py
+++ todos/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Todo
 
 # This views renders all of the current todos on screen.
 def index(request):
-    return HttpResponse("Hello, world. You're at the todos index.")
+    all_todos = Todo.objects.all()
+    context = { 'todos': all_todos }
+    return render(request, 'todos/index.html', context)
 
 def edit(request, todo_id):
     return HttpResponse('Hey, you want to edit todo %s' % todo_id)
