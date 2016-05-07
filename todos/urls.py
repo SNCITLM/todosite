b816cb1064a69cdbba0aa0ac8fc8e71738f82f9f
Leonardo Hahn <leo_lh324@hotmail.com>
2016-05-07 03:25:29 +0200

    list all view working

--- todos/urls.py
+++ todos/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^about/', views.about, name='about'),
-    url(r'^index', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^about$', views.about, name='about'),
     url(r'^(?P<todo_id>[0-9]+)/edit$', views.edit, name='edit'),
-    url(r'^.*$', views.index, name='index'),
 ]
