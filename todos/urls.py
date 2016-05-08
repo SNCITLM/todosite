382e705134d00c41c0eba893adb2c2644a8a33e0
Max Franke <m.franke@campus.tu-berlin.de>
2016-05-08 19:33:50 +0200

    Edit working

--- todos/urls.py
+++ todos/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
 from . import views
-
+app_name = 'todos'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^about$', views.about, name='about'),
-    url(r'^(?P<todo_id>[0-9]+)/edit$', views.edit, name='edit'),
+    url(r'^index', views.index, name='index'),
+    url(r'^about', views.about, name='about'),
+    url(r'^(?P<todo_id>[0-9]+)/edit', views.edit, name='edit'),
+    url(r'^(?P<todo_id>[0-9]+)/save', views.save, name='save'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404,render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from dateutil.parser import parse
 from .models import Todo
 
 # This views renders all of the current todos on screen.
@@ -10,7 +11,48 @@ def index(request):
     return render(request, 'todos/index.html', context)
 
 def edit(request, todo_id):
-    return HttpResponse('Hey, you want to edit todo %s' % todo_id)
+    cur_todo = get_object_or_404(Todo, pk = todo_id)
+    return render(request, 'todos/edit.html', {'todo': cur_todo})
 
 def about(request):
     return HttpResponse("This site was created by Leonardo Hahn and Max Franke")
+
+def save(request, todo_id):
+    cur_todo = get_object_or_404(Todo, pk=todo_id)
+    
+    new_description = request.POST['description']
+    if len(new_description) == 0:
+        return render(request, 'todos/edit.html', {
+            'todo': cur_todo,
+            'error_message': "Description can't be empty",
+    })
+
+    new_due_date = request.POST['due_date']
+    if len(new_due_date) == 0:
+        return render(request, 'todos/edit.html', {
+            'todo': cur_todo,
+            'error_message': "Due Date can't be empty",
+    })
+
+    new_progress = request.POST['progress']
+    if len(new_progress) == 0:
+        return render(request, 'todos/edit.html', {
+            'todo': cur_todo,
+            'error_message': "Progress can't be empty",
+    })
+
+    try:
+        cur_todo.due_date = parse(new_due_date)
+    except ValueError :
+       return render(request, 'todos/edit.html', {
+            'todo': cur_todo,
+            'error_message': "Invalid Date",
+        })
+
+    cur_todo.description = new_description
+    cur_todo.progress = new_progress
+
+    cur_todo.save()
+
+    return HttpResponseRedirect(reverse ('todos:index'))
+
