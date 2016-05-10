1954cb5724d4ea0d309099dda091e9c011ab731d
Leonardo Hahn <leo_lh324@hotmail.com>
2016-05-10 18:22:53 +0200

    almost ready, needs a better about page

--- todos/urls.py
+++ todos/urls.py
@@ -3,11 +3,9 @@ from django.conf.urls import url
 from . import views
 app_name = 'todos'
 urlpatterns = [
-    url(r'^index', views.index, name='index'),
-    url(r'^new', views.new, name='new'),
-    url(r'^add', views.add, name='add'),
+    url(r'^$', views.index, name='index'),
     url(r'^about', views.about, name='about'),
+    url(r'^new', views.new, name='new'),
     url(r'^(?P<todo_id>[0-9]+)/edit', views.edit, name='edit'),
-    url(r'^(?P<todo_id>[0-9]+)/save', views.save, name='save'),
     url(r'^(?P<todo_id>[0-9]+)/delete', views.delete, name='delete'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -11,82 +11,82 @@ def index(request):
     return render(request, 'todos/index.html', context)
 
 def edit(request, todo_id):
-    cur_todo = get_object_or_404(Todo, pk = todo_id)
-    return render(request, 'todos/edit.html', {'todo': cur_todo})
+    current_todo = get_object_or_404(Todo, pk = todo_id)
+    if request.method == 'POST':
+        return update_todo(request, current_todo)
+    else:
+        return render(request, 'todos/edit.html', {'todo': current_todo})
 
 def about(request):
-    return HttpResponse("This site was created by Leonardo Hahn and Max Franke")
+    return HttpResponse("This site was created by Leonardo Hahn and Max Franke.")
 
-def save(request, todo_id):
-    cur_todo = get_object_or_404(Todo, pk=todo_id)
-
+def update_todo(request, todo):
     new_description = request.POST['description']
     if len(new_description) == 0:
         return render(request, 'todos/edit.html', {
-            'todo': cur_todo,
-            'error_message': "Description can't be empty",
-    })
+            'todo': todo,
+            'error_message': "Description can't be empty.",
+        })
 
     new_due_date = request.POST['due_date']
     if len(new_due_date) == 0:
         return render(request, 'todos/edit.html', {
-            'todo': cur_todo,
-            'error_message': "Due Date can't be empty",
-    })
+            'todo': todo,
+            'error_message': "Due Date cannot be empty.",
+        })
 
     new_progress = request.POST['progress']
     if len(new_progress) == 0 or 0 > int(new_progress) > 100:
         return render(request, 'todos/edit.html', {
-            'todo': cur_todo,
-            'error_message': "Progress can't be empty or negtive",
-    })
-
-    try:
-        cur_todo.due_date = parse(new_due_date)
-    except ValueError :
-       return render(request, 'todos/edit.html', {
-            'todo': cur_todo,
-            'error_message': "Invalid Date",
+            'todo': todo,
+            'error_message': "Progress can't be empty and should be a percentage.",
         })
 
-    cur_todo.description = new_description
-    cur_todo.progress = new_progress
+    try:
+        todo.due_date = parse(new_due_date)
+    except ValueError:
+       return render(request, 'todos/edit.html', {
+            'todo': todo,
+            'error_message': "Invalid date.",
+        })
 
-    cur_todo.save()
+    todo.description = new_description
+    todo.progress = new_progress
 
-    return HttpResponseRedirect(reverse ('todos:index'))
+    todo.save()
+
+    return HttpResponseRedirect(reverse('todos:index'))
 
 def delete(request, todo_id):
     Todo.objects.get(pk=todo_id).delete()
-    return HttpResponseRedirect(reverse ('todos:index'))
+    return HttpResponseRedirect(reverse('todos:index'))
 
 def new(request):
-    return render(request, 'todos/new.html')
+    if request.method == 'POST':
+        return new_todo(request)
+    else:
+        return render(request, 'todos/new.html')
 
-def add(request):
-
+def new_todo(request):
     new_description = request.POST['description']
     if len(new_description) == 0:
-        return render(request, 'todos/edit.html', {
-            'todo': cur_todo,
+        return render(request, 'todos/new.html', {
             'error_message': "Description can't be empty",
-    })
+        })
 
     new_due_date = request.POST['due_date']
     if len(new_due_date) == 0:
-        return render(request, 'todos/edit.html', {
-            'todo': cur_todo,
+        return render(request, 'todos/new.html', {
             'error_message': "Due Date can't be empty",
-    })
+        })
 
     try:
         parsed_due_date = parse(new_due_date)
     except ValueError :
-       return render(request, 'todos/edit.html', {
-            'todo': cur_todo,
+       return render(request, 'todos/new.html', {
             'error_message': "Invalid Date",
         })
 
     new_todo = Todo(description=new_description,due_date=parsed_due_date)
     new_todo.save()
-    return HttpResponseRedirect(reverse ('todos:index'))
+    return HttpResponseRedirect(reverse('todos:index'))
